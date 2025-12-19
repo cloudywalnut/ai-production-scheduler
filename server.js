37a38c19c3acebd38cdb91ff11e5400877593acb
Mustansir Muhammad <tp078682@mail.apu.edu.my>
2025-12-19 19:35:45 +0800

    Fix to store pdf properly

--- server.js
+++ server.js
@@ -244,8 +244,8 @@ app.post("/upload", upload.single("script"), async (req, res) => {
   console.log("File size (bytes):", req.file.size);
 
   // Convert buffer to stream if needed
-  fs.writeFileSync("/Vodstr/tempMemory/temp.pdf", req.file.buffer);
-  const pdfStream = fs.createReadStream("/Vodstr/tempMemory/temp.pdf");
+  fs.writeFileSync("temp.pdf", req.file.buffer);
+  const pdfStream = fs.createReadStream("temp.pdf");
   const extractedData = await extractData(pdfStream);
   const scheduledData = await scheduleScenes(extractedData.scenes,7);
   
