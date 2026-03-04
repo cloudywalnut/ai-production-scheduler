6adcae05589819225e62ea6c5e6f2b930e23bddb
Mustansir <mustansir.betul@gmail.com>
2026-03-05 00:04:21 +0800

    App run and PORT fixed

--- server.js
+++ server.js
@@ -487,5 +487,5 @@ app.post("/voice", upload.single("audio"), async (req, res) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running: http://localhost:${PORT}`);
 });
