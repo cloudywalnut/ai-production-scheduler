337375733d4eb23a4ab8458bad9142c9bde55dda
Mustansir <mustansir.betul@gmail.com>
2026-03-04 23:39:49 +0800

    CORS error fix

--- server.js
+++ server.js
@@ -485,7 +485,7 @@ app.post("/voice", upload.single("audio"), async (req, res) => {
 
 });
 
-
-app.listen(5000, () => {
-  console.log("Server running on http://localhost:5000");
+const PORT = process.env.PORT || 5000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
